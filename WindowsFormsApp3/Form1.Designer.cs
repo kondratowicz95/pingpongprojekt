14ee4df4a30867ba47445682ffc84b120ebc75af
projket - pingpong1 <kondratowicz95@gmail.com>
2017-06-19 09:24:28 +0200

    DRUGI COMMIT

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.points_lbl = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racked = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racked)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Controls.Add(this.label1);
+            this.background.Controls.Add(this.points_lbl);
+            this.background.Controls.Add(this.score_lbl);
+            this.background.Controls.Add(this.ball);
+            this.background.Controls.Add(this.racked);
+            this.background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(539, 261);
+            this.background.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 200);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GAME OVER\r\n\r\nF1 -RESTART\r\nESC - EXIT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // points_lbl
+            // 
+            this.points_lbl.AutoSize = true;
+            this.points_lbl.Font = new System.Drawing.Font("Arial Narrow", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points_lbl.Location = new System.Drawing.Point(85, 1);
+            this.points_lbl.Name = "points_lbl";
+            this.points_lbl.Size = new System.Drawing.Size(34, 42);
+            this.points_lbl.TabIndex = 3;
+            this.points_lbl.Text = "0";
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.score_lbl.Location = new System.Drawing.Point(-8, 0);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(98, 43);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score:";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ball.Location = new System.Drawing.Point(12, 46);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // racked
+            // 
+            this.racked.BackColor = System.Drawing.Color.Maroon;
+            this.racked.Location = new System.Drawing.Point(92, 174);
+            this.racked.Name = "racked";
+            this.racked.Size = new System.Drawing.Size(200, 20);
+            this.racked.TabIndex = 0;
+            this.racked.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 261);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.background.ResumeLayout(false);
+            this.background.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racked)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox racked;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label points_lbl;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int points= 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            racked.Top = background.Bottom - (background.Bottom / 10);
+
+            label1.Left = (background.Width / 2) - (label1.Width / 2);
+            label1.Top = (background.Height / 2) - (label1.Height / 2);
+            label1.Visible = false;  // ukrywa strone label1 czyli gameover
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racked.Left = Cursor.Position.X - (racked.Width / 2);
+
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if (ball.Bottom >= racked.Top && ball.Bottom <= racked.Bottom && ball.Left >= racked.Left && ball.Right <= racked.Right) //odbicie piłki przez poprzeczkę
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top; // zmiana kierunku piłki 
+                points += 1; // dodaje punkt
+                points_lbl.Text = points.ToString();
+
+             
+
+            }
+
+            if (ball.Left <= background.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= background.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= background.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= background.Bottom)
+            {
+                timer1.Enabled = false;
+                label1.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+            if(e.KeyCode == Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                points_lbl.Text = "0";
+                timer1.Enabled = true;
+                background.Visible = false;
+
+                 
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
